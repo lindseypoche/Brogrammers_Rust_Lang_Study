36951658b39fa16106b005e1969d9c506ab2b1b4
lindseypoche <lindsey.poche-2@selu.edu>
2020-10-05 23:51:52 -0500

    p4( still working on php file)

---
+++ public_html/P4.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <body>
+            <?php
+            echo $_GET["fname"];
+            echo $_GET["lname"];
+            echo $_GET["q1"];
+            echo $_GET["q2"];
+            echo $_GET["q3"];
+            echo $_GET["q4"];
+            echo $_GET["q5"];
+            echo $_GET["q6"];
+            echo $_GET["q7"];
+            echo $_GET["q8"];
